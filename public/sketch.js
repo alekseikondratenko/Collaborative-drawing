ff5accda925fefa25a5839795001675546d43033
Aleksei Kondratenko <alekseikondratenko7@gmail.com>
2022-07-04 16:47:03 +0300

    Updated the sketch

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,15 @@
 var socket
 
 function setup() {
-  createCanvas(200, 200)
+  createCanvas(600, 400)
+  background(51)
 }
 
 // Socket client side (opening the connection)
 socket = io.connect('http://localhost:3000')
 
 function draw() {
-  background(51)
-  ellipse(mouseX, mouseY, 60, 60)
+  noStroke()
+  fill(255)
+  ellipse(mouseX, mouseY, 36, 36)
 }
